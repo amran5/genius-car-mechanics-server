0fc52debc4a746f72b978009f5a58e3842270bd8
amran5 <amran40004@gmail.com>
2021-10-28 20:49:29 +0300

    commit port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const ObJectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
